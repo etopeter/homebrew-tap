388d1adc5aea2ca966c45218ede0970fcf2537e2
Peter Strzyzewski <peters2222@gmail.com>
2019-09-02 10:56:48 -0700

    Changed dependency to recommended for more flexibility

--- kubectl-view-utilization.rb
+++ kubectl-view-utilization.rb
@@ -4,11 +4,10 @@ class KubectlViewUtilization < Formula
   url "https://github.com/etopeter/kubectl-view-utilization/releases/download/v0.3.0/kubectl-view-utilization-v0.3.0.tar.gz"
   sha256 "051ed3164e6c32c0a2da6913b59e19382b384f1a3ac10741af9ff2a5ab7d8edd"
 
-  depends_on "kubectl"
+  depends_on "kubectl" => :recommended
   depends_on "gawk" => :recommended
 
   def install
     bin.install "kubectl-view-utilization"
   end
-
 end
