dd326af3bc7185580cbc9854c09f8f702a2c0d4f
Peter Strzyzewski <peters2222@gmail.com>
2019-10-14 08:43:43 -0700

    release v0.3.1

--- kubectl-view-utilization.rb
+++ kubectl-view-utilization.rb
@@ -1,8 +1,8 @@
 class KubectlViewUtilization < Formula
   desc "kubectl plugin to show cluster CPU and Memory requests utilization"
   homepage "https://github.com/etopeter/kubectl-view-utilization"
-  url "https://github.com/etopeter/kubectl-view-utilization/releases/download/v0.3.0/kubectl-view-utilization-v0.3.0.tar.gz"
-  sha256 "051ed3164e6c32c0a2da6913b59e19382b384f1a3ac10741af9ff2a5ab7d8edd"
+  url "https://github.com/etopeter/kubectl-view-utilization/releases/download/v0.3.1/kubectl-view-utilization-v0.3.1.tar.gz"
+  sha256 "b0bf83bcec96a10856826fe8f51542559e73c722c1fedfbe4664f46c662e7a0b"
 
   depends_on "kubectl" => :recommended
   depends_on "gawk" => :recommended
